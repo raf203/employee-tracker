8abe6a8328272222dcf84246636a3d08f5806c25
Rafael Cardoso <rrafa203@gmail.com>
2021-12-10 14:13:26 -0800

    add user prompt and functions to server

--- server.js
+++ server.js
@@ -1,14 +1,107 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const consoleTable = require('console.table');
+const cTable = require('console.table');
 const db = mysql.createConnection(
+//acess to database
     {
       host: 'localhost',
-      // Your MySQL username,
       user: 'root',
-      // Your MySQL password
       password: 'Rafocean230321@',
       database: 'employee_tracker_db'
     },
     console.log('Connected to employee_tracker_db')
 );
+
+//error
+db.connect(function(err) {
+    if (err) throw err;
+    promptUser();
+  });
+
+// Menu 
+function promptUser() {
+    return inquirer.prompt([
+    {
+    type: 'list',
+    message: 'Choose an option:',
+    name: 'action',
+    choices: [
+            'view all employees', 
+            'view all departments',
+            'view all roles',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update the employee role',
+            'exit'
+            ]
+    }
+]).then(function(answers) {
+        switch (answers.action) {
+            case 'view all employees':
+                allEmployees();
+            break;
+
+            case 'view all departments':
+                allDepartments();
+            break;
+
+            case 'view all roles':
+                allRoles();
+            break;
+
+            case 'add departement':
+                addDepartment();
+            break;
+
+            case 'add role':
+                addRole();
+            break;
+
+            case 'add employee':
+                addEmployee();
+            break;
+
+            case 'update employee role':
+                updateRole();
+            break;
+
+            case 'exit':
+                break;    
+         }
+    })
+};
+
+function allEmployees(){
+    const sql = `SELECT * FROM employee`
+    db.query(sql, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        promptUser()
+      });
+}
+
+function allDepartments(){
+
+}
+
+function allRoles(){
+
+}
+
+function addDepartment(){
+
+}
+
+function addEmployee(){
+
+}
+
+function addRole(){
+
+}
+
+function updateRole(){
+
+} 
+
